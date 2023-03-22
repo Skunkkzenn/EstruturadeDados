95c3fa6d629305a3f29440f363a6746eaf39c59a
Victor <a18586@alunos.ipca.pt>
2023-03-22 22:50:57 +0000

    push e alteração clientes

---
+++ Projeto_EstruturadeDados/Cliente.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#pragma warning( disable : 4996 ) 
+#define M 200
+
+typedef struct Cliente {
+	int cod;
+	char nome[M];
+	float saldo;
+	long int nif;
+	char morada[M];
+	struct Cliente* next;
+}Cliente;
