bb5904b7a90ecdb2173e7d1be2e0b6d518b87acf
Victor <a18586@alunos.ipca.pt>
2023-03-22 17:36:09 +0000

    Metodo Verifica Veiculo Duplicado criado

--- Projeto_EstruturadeDados/Veiculo.c
+++ Projeto_EstruturadeDados/Veiculo.c
@@ -78,5 +78,60 @@ Veiculo* InsertVeiculoFim(Veiculo* novo, Veiculo* inicio) {
 	return inicio;
 }
 
+// Insere Veiculo Lista
+Veiculo* InsertVeiculoLista(Veiculo* novo, Veiculo* inicio) {
+
+	if (inicio == NULL)
+	{
+		inicio = novo;
+	}
+	else
+	{
+		Veiculo* aux = inicio;
+		Veiculo* auxSup = NULL;
+
+		//Verificar se já existe repetido
+		if (VerificaVeiculoDuplicado(novo->bateria, novo->tipo, inicio) == inicio)
+			return inicio;
+
+		//procurar a posicao correta, e ordena pelo cod!!!!
+		while (aux && aux->cod < novo->cod) {
+			auxSup = aux;
+			aux = aux->next;
+		}
+
+		if (auxSup == NULL && novo->cod < inicio->cod) { //Insere no início
+			novo->next = inicio;
+			inicio = novo;
+		}
+		else if (auxSup == NULL)
+		{ //Insere no meio
+			novo->next = aux;
+			inicio = novo;
+		}
+		else //Insere no fim
+		{
+			auxSup->next = novo;
+			novo->next = aux;
+		}
+		return inicio;
+	}
+
+}
+
+// Verifica Veiculo em duplicado
+Veiculo* VerificaVeiculoDuplicado(int cod, char* tipo, Veiculo* inicio) {
+	if (inicio == NULL) // Verifica se a lista encadeada está vazia (inicio == NULL), caso esteja retorna NULL, indicando que não há veículos duplicados.
+	{
+		return NULL; // Caso não encontre nenhum veículo com o mesmo código e tipo, retorna NULL.
+	}
+
+	Veiculo* aux = inicio;
+	while ((aux != NULL) && (aux->cod != cod) && (strcmp(aux->tipo, tipo) != 0)) // Percorre a lista encadeada, enquanto não chegar ao final da lista (aux != NULL) e não encontrar um veículo com o mesmo código e tipo (aux->cod != cod) && (strcmp(aux->tipo, tipo) != 0). Se encontrar um veículo com o mesmo código e tipo, retorna um ponteiro para esse veículo (aux).
+	{
+		aux = aux->next;
+	}
+	return aux;
+}
 
 #pragma endregion
