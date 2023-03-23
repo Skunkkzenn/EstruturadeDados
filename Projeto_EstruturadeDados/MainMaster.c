7870599a14b337bf7a601c32bedfb46d436394f9
Victor <a18586@alunos.ipca.pt>
2023-03-23 14:33:27 +0000

    criação de main para aplicação das estruturas e funções

---
+++ Projeto_EstruturadeDados/MainMaster.c
@@ -0,0 +1,16 @@
+/*****************************************************************//**
+ * \file   MainMaster.c
+ * \brief  TRABALHO EDA
+ * \number 18586
+ * \author Victor Destefani
+ * \date   March 2023
+ *********************************************************************/
+
+#include <stdio.h>
+#include <string.h> // para as strings
+#include <stdbool.h>
+#include <stdlib.h>
+#include "Veiculo.h"
+#include "Cliente.h"
+#include "Gestor.h"
+#pragma warning( disable : 4996 )
