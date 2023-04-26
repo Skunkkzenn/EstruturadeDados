f1fc9100da5c76ab44b1b21e9ff1cbb892771e31
Victor <a18586@alunos.ipca.pt>
2023-04-26 23:10:05 +0100

    Criação de métodos e assinaturas, inserção de funções no main

--- Projeto_EstruturadeDados/Grafo.h
+++ Projeto_EstruturadeDados/Grafo.h
@@ -17,3 +17,6 @@ typedef struct Vertice {
 }Vertice;
 
 
+bool CriaGrafo(int numVertices, float mat[][MAX]);
+bool InsereVertice(Vertice v[], int* numVert, char* local);
+bool InsereAresta(float ma[][MAX], int totVertices, Vertice v[], int numVertices, char* origem, char* destino, float p);
