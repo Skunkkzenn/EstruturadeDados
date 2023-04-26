4a62ce240fb76045ffeea56c3a15940e45ca484a
Victor <a18586@alunos.ipca.pt>
2023-04-26 22:40:00 +0100

    Criar estrutura de grafos

---
+++ Projeto_EstruturadeDados/Grafo.h
@@ -0,0 +1,12 @@
+/*****************************************************************//**
+ * \file   Grafo.h
+ * \brief  TRABALHO EDA
+ * \number 18586
+ * \author Victor Destefani
+ * \date   April 2023
+ *********************************************************************/
+#pragma once
+
+#include <stdbool.h>
+#define MAX 10
+#define N 20
